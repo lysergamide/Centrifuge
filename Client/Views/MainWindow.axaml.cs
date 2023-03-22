f6a251ca527213dacadabaa1bafc9f78f723c622
Box of Rain <34106048+lysergamide@users.noreply.github.com>
2023-03-21 20:22:53 -0700

    Add TabStrip (#1)
    Add a custom tabstrip and tab view for the side bar

--- Client/Views/MainWindow.axaml.cs
+++ Client/Views/MainWindow.axaml.cs
@@ -1,11 +1,12 @@
+﻿using Avalonia;
 using Avalonia.Controls;
 
-namespace Centrifuge.Client;
-
+namespace Centrifuge.Client.Views;
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        LeftTabStrip.AddFixedTab("Default");
     }
 }
---
+++ Client/Views/Tab.axaml.cs
@@ -0,0 +1,53 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Centrifuge.Client.Views;
+
+/// <summary>
+/// Tab control used by tab strips
+/// </summary>
+public partial class Tab : UserControl
+{
+    private bool hideDeleteButton;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Tab"/> class.
+    /// </summary>
+    public Tab()
+    {
+        InitializeComponent();
+        DeleteButton.Click += delegate
+        {
+            RaiseEvent(new RoutedEventArgs { RoutedEvent = DeleteEvent });
+        };
+    }
+
+    /// <summary>
+    /// The text displayed on the tab
+    /// </summary>
+    public string Label
+    {
+        get => TextBlock.Text;
+        set => TextBlock.Text = value;
+    }
+
+    /// <summary>
+    /// Whether or not the tab is fixed <br/>
+    /// If set to true the tab's delete button will be hidden
+    /// </summary>
+    public bool Fixed
+    {
+        get => hideDeleteButton;
+        set
+        {
+            if (hideDeleteButton == value)
+                return;
+
+            hideDeleteButton = value;
+            var buttonWidth = hideDeleteButton ? new GridLength(0) : new GridLength(1, GridUnitType.Star);
+            TabGrid.ColumnDefinitions[1].Width = buttonWidth;
+        }
+    }
+
+    public static RoutedEvent<RoutedEventArgs> DeleteEvent =
+        RoutedEvent.Register<RoutedEventArgs>("DeleteTab", RoutingStrategies.Bubble, typeof(Tab));
+}
