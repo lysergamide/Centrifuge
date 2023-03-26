07b8be9866f1464222238ff3ff3f74391bb21392
lysergamide <box@tewi.pw>
2023-03-26 01:43:45 -0700

    Add DB context and models for client's db

---
+++ Models/DBctx.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Centrifuge.Models;
+
+public class LocalDbContext : DbContext
+{
+    public DbSet<User> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    private static readonly string connectionString =
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "Centrifuge",
+            "local.db"
+        );
+
+    public LocalDbContext()
+    {
+        if (!File.Exists(connectionString) && connectionString != null)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(connectionString));
+            File.Create(connectionString).Close();
+        }
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={connectionString}");
+    }
+}
+
--- Models/User.cs
+++ Models/User.cs
@@ -1,22 +1,19 @@
-using System;
-using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Centrifuge.Models;
 
-[Index(nameof(Username), IsUnique=true)]
+[Index(nameof(Name), IsUnique = true)]
 public class User
 {
+    public int Id { get; set; }
+
     // Use Regex = ^[a-z](-?[a-z]){1,23}$
-//        public int ID { get; set; }
-
+    //        public int ID { get; set; }
     [Required]
-    public string Username { get; set; }
-
+    public string Name { get; set; }
     public DateTime Registered { get; set; }
     public DateTime LastActive { get; set; }
-    
+
     //public virtual ICollection<Enrollment> Enrollments { get; set; }
 }
